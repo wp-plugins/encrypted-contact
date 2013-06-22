528f08df15d79b4d2eb95b73547ddca566402d4f
kerrylinux <kerrylinux@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-22 21:35:51 +0000

    Update kerrylinuxauth.php for non-SSL websites
    git-svn-id: https://plugins.svn.wordpress.org/encrypted-contact/trunk@730097 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kerrylinuxauth.php
+++ kerrylinuxauth.php
@@ -2,12 +2,18 @@
 // kerrylinuxauth.php for Wordpress
 // application specific code that sets the key directory to admin
 
-$GPGDIR = $GPGDIR."/admin";
+
 if (! is_dir($GPGDIR)) {
-      mkdir($GPGDIR,0700);
-}
-if (! is_dir($GPGDIR."/messages")) {
-      mkdir($GPGDIR."/messages",0700);
+     echo "There is no directory $GPGDIR to store the keys.\n\n";
+     echo "Run the following commands as root :  mkdir $GPGDIR; chown apache $GPGDIR";
+} else {
+     $GPGDIR = $GPGDIR."/admin";
+     if (! is_dir($GPGDIR)) {
+          mkdir($GPGDIR,0700);
+     }
+     if (! is_dir($GPGDIR."/messages")) {
+          mkdir($GPGDIR."/messages",0700);
+     }
 }
 
 ?>
